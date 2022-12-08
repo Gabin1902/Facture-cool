5238af26242afb4b749519859aa0834093a68077
EKOUDON Gabin <gekoudon@gmail.com>
2022-12-08 12:56:55 +0100

    Ajout du parametre calcul

--- accueil.c
+++ accueil.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 int main(){
     printf("wellcome to apps\n");
+    printf("%d", addition(11,10));
 }
--- calcul.c
+++ calcul.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
 int addition(int val1, int val2){
-    
+    int re;
+    re=val1+val2;
+    return re;
 }
